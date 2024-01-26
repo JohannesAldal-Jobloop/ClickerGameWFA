519b10df86b2c6a6875ad106e2de66e19530aae5
JohannesAldal-Jobloop <johannes@jobloop.no>
2024-01-26 15:17:38 +0100

    Added a counter and upgrade buttons

--- ClickerGame/ClickerGame/Form1.Designer.cs
+++ ClickerGame/ClickerGame/Form1.Designer.cs
@@ -28,13 +28,305 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ClickerButton = new System.Windows.Forms.Button();
+            this.ClickerCounter = new System.Windows.Forms.Label();
+            this.Upgrade1 = new System.Windows.Forms.Button();
+            this.Upgrade2 = new System.Windows.Forms.Button();
+            this.Upgrade7 = new System.Windows.Forms.Button();
+            this.Upgrade6 = new System.Windows.Forms.Button();
+            this.Upgrade5 = new System.Windows.Forms.Button();
+            this.Upgrade4 = new System.Windows.Forms.Button();
+            this.Upgrade3 = new System.Windows.Forms.Button();
+            this.Upgrade9 = new System.Windows.Forms.Button();
+            this.Upgrade8 = new System.Windows.Forms.Button();
+            this.Upgrade17 = new System.Windows.Forms.Button();
+            this.Upgrade18 = new System.Windows.Forms.Button();
+            this.Upgrade12 = new System.Windows.Forms.Button();
+            this.Upgrade13 = new System.Windows.Forms.Button();
+            this.Upgrade14 = new System.Windows.Forms.Button();
+            this.Upgrade15 = new System.Windows.Forms.Button();
+            this.Upgrade16 = new System.Windows.Forms.Button();
+            this.Upgrade11 = new System.Windows.Forms.Button();
+            this.Upgrade10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ClickerButton
+            // 
+            this.ClickerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClickerButton.Location = new System.Drawing.Point(355, 200);
+            this.ClickerButton.Name = "ClickerButton";
+            this.ClickerButton.Size = new System.Drawing.Size(70, 70);
+            this.ClickerButton.TabIndex = 0;
+            this.ClickerButton.Text = "Get points";
+            this.ClickerButton.UseVisualStyleBackColor = true;
+            this.ClickerButton.Click += new System.EventHandler(this.ClickerButton_Click);
+            // 
+            // ClickerCounter
+            // 
+            this.ClickerCounter.AutoSize = true;
+            this.ClickerCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClickerCounter.Location = new System.Drawing.Point(363, 139);
+            this.ClickerCounter.Name = "ClickerCounter";
+            this.ClickerCounter.Size = new System.Drawing.Size(53, 58);
+            this.ClickerCounter.TabIndex = 1;
+            this.ClickerCounter.Text = "0";
+            this.ClickerCounter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Upgrade1
+            // 
+            this.Upgrade1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade1.Location = new System.Drawing.Point(582, 12);
+            this.Upgrade1.Name = "Upgrade1";
+            this.Upgrade1.Size = new System.Drawing.Size(100, 40);
+            this.Upgrade1.TabIndex = 2;
+            this.Upgrade1.Text = "Auto points 1";
+            this.Upgrade1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Upgrade1.UseVisualStyleBackColor = true;
+            this.Upgrade1.Click += new System.EventHandler(this.Upgrade1_Click);
+            // 
+            // Upgrade2
+            // 
+            this.Upgrade2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade2.Location = new System.Drawing.Point(582, 58);
+            this.Upgrade2.Name = "Upgrade2";
+            this.Upgrade2.Size = new System.Drawing.Size(100, 40);
+            this.Upgrade2.TabIndex = 3;
+            this.Upgrade2.Text = "Double clicks";
+            this.Upgrade2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Upgrade2.UseVisualStyleBackColor = true;
+            this.Upgrade2.Click += new System.EventHandler(this.Upgrade2_Click);
+            // 
+            // Upgrade7
+            // 
+            this.Upgrade7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade7.Location = new System.Drawing.Point(582, 288);
+            this.Upgrade7.Name = "Upgrade7";
+            this.Upgrade7.Size = new System.Drawing.Size(100, 40);
+            this.Upgrade7.TabIndex = 4;
+            this.Upgrade7.Text = "Upgrade 7";
+            this.Upgrade7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Upgrade7.UseVisualStyleBackColor = true;
+            // 
+            // Upgrade6
+            // 
+            this.Upgrade6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade6.Location = new System.Drawing.Point(582, 242);
+            this.Upgrade6.Name = "Upgrade6";
+            this.Upgrade6.Size = new System.Drawing.Size(100, 40);
+            this.Upgrade6.TabIndex = 5;
+            this.Upgrade6.Text = "Upgrade 6";
+            this.Upgrade6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Upgrade6.UseVisualStyleBackColor = true;
+            // 
+            // Upgrade5
+            // 
+            this.Upgrade5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade5.Location = new System.Drawing.Point(582, 196);
+            this.Upgrade5.Name = "Upgrade5";
+            this.Upgrade5.Size = new System.Drawing.Size(100, 40);
+            this.Upgrade5.TabIndex = 6;
+            this.Upgrade5.Text = "Upgrade 5";
+            this.Upgrade5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Upgrade5.UseVisualStyleBackColor = true;
+            // 
+            // Upgrade4
+            // 
+            this.Upgrade4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade4.Location = new System.Drawing.Point(582, 150);
+            this.Upgrade4.Name = "Upgrade4";
+            this.Upgrade4.Size = new System.Drawing.Size(100, 40);
+            this.Upgrade4.TabIndex = 7;
+            this.Upgrade4.Text = "Upgrade 4";
+            this.Upgrade4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Upgrade4.UseVisualStyleBackColor = true;
+            // 
+            // Upgrade3
+            // 
+            this.Upgrade3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade3.Location = new System.Drawing.Point(582, 104);
+            this.Upgrade3.Name = "Upgrade3";
+            this.Upgrade3.Size = new System.Drawing.Size(100, 40);
+            this.Upgrade3.TabIndex = 8;
+            this.Upgrade3.Text = "Upgrade 3";
+            this.Upgrade3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Upgrade3.UseVisualStyleBackColor = true;
+            this.Upgrade3.Click += new System.EventHandler(this.Upgrade3_Click);
+            // 
+            // Upgrade9
+            // 
+            this.Upgrade9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade9.Location = new System.Drawing.Point(582, 380);
+            this.Upgrade9.Name = "Upgrade9";
+            this.Upgrade9.Size = new System.Drawing.Size(100, 40);
+            this.Upgrade9.TabIndex = 9;
+            this.Upgrade9.Text = "Upgrade 9";
+            this.Upgrade9.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Upgrade9.UseVisualStyleBackColor = true;
+            // 
+            // Upgrade8
+            // 
+            this.Upgrade8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade8.Location = new System.Drawing.Point(582, 334);
+            this.Upgrade8.Name = "Upgrade8";
+            this.Upgrade8.Size = new System.Drawing.Size(100, 40);
+            this.Upgrade8.TabIndex = 10;
+            this.Upgrade8.Text = "Upgrade 8";
+            this.Upgrade8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Upgrade8.UseVisualStyleBackColor = true;
+            // 
+            // Upgrade17
+            // 
+            this.Upgrade17.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade17.Location = new System.Drawing.Point(688, 334);
+            this.Upgrade17.Name = "Upgrade17";
+            this.Upgrade17.Size = new System.Drawing.Size(100, 40);
+            this.Upgrade17.TabIndex = 19;
+            this.Upgrade17.Text = "Upgrade 17";
+            this.Upgrade17.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Upgrade17.UseVisualStyleBackColor = true;
+            // 
+            // Upgrade18
+            // 
+            this.Upgrade18.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade18.Location = new System.Drawing.Point(688, 380);
+            this.Upgrade18.Name = "Upgrade18";
+            this.Upgrade18.Size = new System.Drawing.Size(100, 40);
+            this.Upgrade18.TabIndex = 18;
+            this.Upgrade18.Text = "Upgrade 18";
+            this.Upgrade18.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Upgrade18.UseVisualStyleBackColor = true;
+            // 
+            // Upgrade12
+            // 
+            this.Upgrade12.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade12.Location = new System.Drawing.Point(688, 104);
+            this.Upgrade12.Name = "Upgrade12";
+            this.Upgrade12.Size = new System.Drawing.Size(100, 40);
+            this.Upgrade12.TabIndex = 17;
+            this.Upgrade12.Text = "Upgrade 12";
+            this.Upgrade12.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Upgrade12.UseVisualStyleBackColor = true;
+            // 
+            // Upgrade13
+            // 
+            this.Upgrade13.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade13.Location = new System.Drawing.Point(688, 150);
+            this.Upgrade13.Name = "Upgrade13";
+            this.Upgrade13.Size = new System.Drawing.Size(100, 40);
+            this.Upgrade13.TabIndex = 16;
+            this.Upgrade13.Text = "Upgrade 13";
+            this.Upgrade13.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Upgrade13.UseVisualStyleBackColor = true;
+            // 
+            // Upgrade14
+            // 
+            this.Upgrade14.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade14.Location = new System.Drawing.Point(688, 196);
+            this.Upgrade14.Name = "Upgrade14";
+            this.Upgrade14.Size = new System.Drawing.Size(100, 40);
+            this.Upgrade14.TabIndex = 15;
+            this.Upgrade14.Text = "Upgrade 14";
+            this.Upgrade14.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Upgrade14.UseVisualStyleBackColor = true;
+            // 
+            // Upgrade15
+            // 
+            this.Upgrade15.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade15.Location = new System.Drawing.Point(688, 242);
+            this.Upgrade15.Name = "Upgrade15";
+            this.Upgrade15.Size = new System.Drawing.Size(100, 40);
+            this.Upgrade15.TabIndex = 14;
+            this.Upgrade15.Text = "Upgrade 15";
+            this.Upgrade15.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Upgrade15.UseVisualStyleBackColor = true;
+            // 
+            // Upgrade16
+            // 
+            this.Upgrade16.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade16.Location = new System.Drawing.Point(688, 288);
+            this.Upgrade16.Name = "Upgrade16";
+            this.Upgrade16.Size = new System.Drawing.Size(100, 40);
+            this.Upgrade16.TabIndex = 13;
+            this.Upgrade16.Text = "Upgrade 16";
+            this.Upgrade16.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Upgrade16.UseVisualStyleBackColor = true;
+            // 
+            // Upgrade11
+            // 
+            this.Upgrade11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade11.Location = new System.Drawing.Point(688, 58);
+            this.Upgrade11.Name = "Upgrade11";
+            this.Upgrade11.Size = new System.Drawing.Size(100, 40);
+            this.Upgrade11.TabIndex = 12;
+            this.Upgrade11.Text = "Upgrade 11";
+            this.Upgrade11.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Upgrade11.UseVisualStyleBackColor = true;
+            // 
+            // Upgrade10
+            // 
+            this.Upgrade10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade10.Location = new System.Drawing.Point(688, 12);
+            this.Upgrade10.Name = "Upgrade10";
+            this.Upgrade10.Size = new System.Drawing.Size(100, 40);
+            this.Upgrade10.TabIndex = 11;
+            this.Upgrade10.Text = "Upgrade 10";
+            this.Upgrade10.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Upgrade10.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Upgrade17);
+            this.Controls.Add(this.Upgrade18);
+            this.Controls.Add(this.Upgrade12);
+            this.Controls.Add(this.Upgrade13);
+            this.Controls.Add(this.Upgrade14);
+            this.Controls.Add(this.Upgrade15);
+            this.Controls.Add(this.Upgrade16);
+            this.Controls.Add(this.Upgrade11);
+            this.Controls.Add(this.Upgrade10);
+            this.Controls.Add(this.Upgrade8);
+            this.Controls.Add(this.Upgrade9);
+            this.Controls.Add(this.Upgrade3);
+            this.Controls.Add(this.Upgrade4);
+            this.Controls.Add(this.Upgrade5);
+            this.Controls.Add(this.Upgrade6);
+            this.Controls.Add(this.Upgrade7);
+            this.Controls.Add(this.Upgrade2);
+            this.Controls.Add(this.Upgrade1);
+            this.Controls.Add(this.ClickerCounter);
+            this.Controls.Add(this.ClickerButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ClickerButton;
+        private System.Windows.Forms.Label ClickerCounter;
+        private System.Windows.Forms.Button Upgrade1;
+        private System.Windows.Forms.Button Upgrade2;
+        private System.Windows.Forms.Button Upgrade7;
+        private System.Windows.Forms.Button Upgrade6;
+        private System.Windows.Forms.Button Upgrade5;
+        private System.Windows.Forms.Button Upgrade4;
+        private System.Windows.Forms.Button Upgrade3;
+        private System.Windows.Forms.Button Upgrade9;
+        private System.Windows.Forms.Button Upgrade8;
+        private System.Windows.Forms.Button Upgrade17;
+        private System.Windows.Forms.Button Upgrade18;
+        private System.Windows.Forms.Button Upgrade12;
+        private System.Windows.Forms.Button Upgrade13;
+        private System.Windows.Forms.Button Upgrade14;
+        private System.Windows.Forms.Button Upgrade15;
+        private System.Windows.Forms.Button Upgrade16;
+        private System.Windows.Forms.Button Upgrade11;
+        private System.Windows.Forms.Button Upgrade10;
     }
 }
 
--- ClickerGame/ClickerGame/Form1.cs
+++ ClickerGame/ClickerGame/Form1.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,78 @@ namespace ClickerGame
 {
     public partial class Form1 : Form
     {
+        // The variable that stores the points the user have
+        // and how many points per click they get.
+        int points = 0;
+        int pointsPerClick = 1;
+
+        int upgradePrice;
+        int autoPointsPerSecond;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // Aoutomaticly increases points by pointsPerSecond per second.
+        public void AutoPoints( int pointsPerSecond)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Debug.WriteLine("loop");
+                points += pointsPerSecond;
+                ClickerCounter.Text = points.ToString();
+                Thread.Sleep(1000);
+            }
+            
+        }
+
+        // Increases the points by pointsPerClick and displayes
+        // the points in ClickerCounter labes text when the button is clicked. 
+        private void ClickerButton_Click(object sender, EventArgs e)
+        {
+            points += pointsPerClick;
+            ClickerCounter.Text = points.ToString();
+        }
+
+        // If points is bigge ror equals upgradePrice you start the AutoPoints funksjon.
+        private void Upgrade1_Click(object sender, EventArgs e)
+        {
+            upgradePrice = 10;
+
+            if(points >= upgradePrice)
+            {
+                points -= upgradePrice;
+                ClickerCounter.Text = points.ToString();
+                AutoPoints(1);
+                Upgrade1.Enabled = false;
+            }
+        }
+
+        private void Upgrade2_Click(object sender, EventArgs e)
+        {
+            upgradePrice = 10;
+
+            if (points >= upgradePrice)
+            {
+                points -= upgradePrice;
+                ClickerCounter.Text = points.ToString();
+                pointsPerClick *= 2;
+                Upgrade2.Enabled = false;
+            }
+        }
+
+        private void Upgrade3_Click(object sender, EventArgs e)
+        {
+            upgradePrice = 50;
+
+            if (points >= upgradePrice)
+            {
+                points -= upgradePrice;
+                ClickerCounter.Text = points.ToString();
+                pointsPerClick *= 3;
+                Upgrade3.Enabled = false;
+            }
         }
     }
 }
