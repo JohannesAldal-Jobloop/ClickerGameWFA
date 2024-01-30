da98818e44e094cbfe13035a50abf0258e6b71c4
JohannesAldal-Jobloop <johannes@jobloop.no>
2024-01-30 15:38:06 +0100

    Fixed some stuff

--- ClickerGame/ClickerGame/Form1.Designer.cs
+++ ClickerGame/ClickerGame/Form1.Designer.cs
@@ -77,10 +77,11 @@
             // Upgrade1
             // 
             this.Upgrade1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade1.Location = new System.Drawing.Point(582, 12);
             this.Upgrade1.Name = "Upgrade1";
             this.Upgrade1.Size = new System.Drawing.Size(100, 40);
-            this.Upgrade1.TabIndex = 2;
+            this.Upgrade1.TabIndex = 1;
             this.Upgrade1.Text = "Auto points 1";
             this.Upgrade1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade1.UseVisualStyleBackColor = true;
@@ -89,11 +90,12 @@
             // Upgrade2
             // 
             this.Upgrade2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade2.Location = new System.Drawing.Point(582, 58);
             this.Upgrade2.Name = "Upgrade2";
             this.Upgrade2.Size = new System.Drawing.Size(100, 40);
-            this.Upgrade2.TabIndex = 3;
-            this.Upgrade2.Text = "Double clicks";
+            this.Upgrade2.TabIndex = 2;
+            this.Upgrade2.Text = "X2 clicks";
             this.Upgrade2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade2.UseVisualStyleBackColor = true;
             this.Upgrade2.Click += new System.EventHandler(this.Upgrade2_Click);
@@ -101,55 +103,64 @@
             // Upgrade7
             // 
             this.Upgrade7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade7.Location = new System.Drawing.Point(582, 288);
             this.Upgrade7.Name = "Upgrade7";
             this.Upgrade7.Size = new System.Drawing.Size(100, 40);
-            this.Upgrade7.TabIndex = 4;
+            this.Upgrade7.TabIndex = 7;
             this.Upgrade7.Text = "Upgrade 7";
             this.Upgrade7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade7.UseVisualStyleBackColor = true;
+            this.Upgrade7.Click += new System.EventHandler(this.Upgrade7_Click);
             // 
             // Upgrade6
             // 
             this.Upgrade6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade6.Location = new System.Drawing.Point(582, 242);
             this.Upgrade6.Name = "Upgrade6";
             this.Upgrade6.Size = new System.Drawing.Size(100, 40);
-            this.Upgrade6.TabIndex = 5;
+            this.Upgrade6.TabIndex = 6;
             this.Upgrade6.Text = "Upgrade 6";
             this.Upgrade6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade6.UseVisualStyleBackColor = true;
+            this.Upgrade6.Click += new System.EventHandler(this.Upgrade6_Click);
             // 
             // Upgrade5
             // 
             this.Upgrade5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade5.Location = new System.Drawing.Point(582, 196);
             this.Upgrade5.Name = "Upgrade5";
             this.Upgrade5.Size = new System.Drawing.Size(100, 40);
-            this.Upgrade5.TabIndex = 6;
+            this.Upgrade5.TabIndex = 5;
             this.Upgrade5.Text = "Upgrade 5";
             this.Upgrade5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade5.UseVisualStyleBackColor = true;
+            this.Upgrade5.Click += new System.EventHandler(this.Upgrade5_Click);
             // 
             // Upgrade4
             // 
             this.Upgrade4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade4.Location = new System.Drawing.Point(582, 150);
             this.Upgrade4.Name = "Upgrade4";
             this.Upgrade4.Size = new System.Drawing.Size(100, 40);
-            this.Upgrade4.TabIndex = 7;
-            this.Upgrade4.Text = "Upgrade 4";
+            this.Upgrade4.TabIndex = 4;
+            this.Upgrade4.Text = "Auto points 2";
             this.Upgrade4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade4.UseVisualStyleBackColor = true;
+            this.Upgrade4.Click += new System.EventHandler(this.Upgrade4_Click);
             // 
             // Upgrade3
             // 
             this.Upgrade3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade3.Location = new System.Drawing.Point(582, 104);
             this.Upgrade3.Name = "Upgrade3";
             this.Upgrade3.Size = new System.Drawing.Size(100, 40);
-            this.Upgrade3.TabIndex = 8;
-            this.Upgrade3.Text = "Upgrade 3";
+            this.Upgrade3.TabIndex = 3;
+            this.Upgrade3.Text = "+2 base click";
             this.Upgrade3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade3.UseVisualStyleBackColor = true;
             this.Upgrade3.Click += new System.EventHandler(this.Upgrade3_Click);
@@ -157,6 +168,7 @@
             // Upgrade9
             // 
             this.Upgrade9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade9.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade9.Location = new System.Drawing.Point(582, 380);
             this.Upgrade9.Name = "Upgrade9";
             this.Upgrade9.Size = new System.Drawing.Size(100, 40);
@@ -164,32 +176,38 @@
             this.Upgrade9.Text = "Upgrade 9";
             this.Upgrade9.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade9.UseVisualStyleBackColor = true;
+            this.Upgrade9.Click += new System.EventHandler(this.Upgrade9_Click);
             // 
             // Upgrade8
             // 
             this.Upgrade8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade8.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade8.Location = new System.Drawing.Point(582, 334);
             this.Upgrade8.Name = "Upgrade8";
             this.Upgrade8.Size = new System.Drawing.Size(100, 40);
-            this.Upgrade8.TabIndex = 10;
+            this.Upgrade8.TabIndex = 8;
             this.Upgrade8.Text = "Upgrade 8";
             this.Upgrade8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade8.UseVisualStyleBackColor = true;
+            this.Upgrade8.Click += new System.EventHandler(this.Upgrade8_Click);
             // 
             // Upgrade17
             // 
             this.Upgrade17.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade17.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade17.Location = new System.Drawing.Point(688, 334);
             this.Upgrade17.Name = "Upgrade17";
             this.Upgrade17.Size = new System.Drawing.Size(100, 40);
-            this.Upgrade17.TabIndex = 19;
+            this.Upgrade17.TabIndex = 17;
             this.Upgrade17.Text = "Upgrade 17";
             this.Upgrade17.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade17.UseVisualStyleBackColor = true;
+            this.Upgrade17.Click += new System.EventHandler(this.Upgrade17_Click);
             // 
             // Upgrade18
             // 
             this.Upgrade18.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade18.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade18.Location = new System.Drawing.Point(688, 380);
             this.Upgrade18.Name = "Upgrade18";
             this.Upgrade18.Size = new System.Drawing.Size(100, 40);
@@ -197,83 +215,98 @@
             this.Upgrade18.Text = "Upgrade 18";
             this.Upgrade18.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade18.UseVisualStyleBackColor = true;
+            this.Upgrade18.Click += new System.EventHandler(this.Upgrade18_Click);
             // 
             // Upgrade12
             // 
             this.Upgrade12.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade12.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade12.Location = new System.Drawing.Point(688, 104);
             this.Upgrade12.Name = "Upgrade12";
             this.Upgrade12.Size = new System.Drawing.Size(100, 40);
-            this.Upgrade12.TabIndex = 17;
+            this.Upgrade12.TabIndex = 12;
             this.Upgrade12.Text = "Upgrade 12";
             this.Upgrade12.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade12.UseVisualStyleBackColor = true;
+            this.Upgrade12.Click += new System.EventHandler(this.Upgrade12_Click);
             // 
             // Upgrade13
             // 
             this.Upgrade13.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade13.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade13.Location = new System.Drawing.Point(688, 150);
             this.Upgrade13.Name = "Upgrade13";
             this.Upgrade13.Size = new System.Drawing.Size(100, 40);
-            this.Upgrade13.TabIndex = 16;
+            this.Upgrade13.TabIndex = 13;
             this.Upgrade13.Text = "Upgrade 13";
             this.Upgrade13.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade13.UseVisualStyleBackColor = true;
+            this.Upgrade13.Click += new System.EventHandler(this.Upgrade13_Click);
             // 
             // Upgrade14
             // 
             this.Upgrade14.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade14.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade14.Location = new System.Drawing.Point(688, 196);
             this.Upgrade14.Name = "Upgrade14";
             this.Upgrade14.Size = new System.Drawing.Size(100, 40);
-            this.Upgrade14.TabIndex = 15;
+            this.Upgrade14.TabIndex = 14;
             this.Upgrade14.Text = "Upgrade 14";
             this.Upgrade14.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade14.UseVisualStyleBackColor = true;
+            this.Upgrade14.Click += new System.EventHandler(this.Upgrade14_Click);
             // 
             // Upgrade15
             // 
             this.Upgrade15.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade15.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade15.Location = new System.Drawing.Point(688, 242);
             this.Upgrade15.Name = "Upgrade15";
             this.Upgrade15.Size = new System.Drawing.Size(100, 40);
-            this.Upgrade15.TabIndex = 14;
+            this.Upgrade15.TabIndex = 15;
             this.Upgrade15.Text = "Upgrade 15";
             this.Upgrade15.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade15.UseVisualStyleBackColor = true;
+            this.Upgrade15.Click += new System.EventHandler(this.Upgrade15_Click);
             // 
             // Upgrade16
             // 
             this.Upgrade16.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade16.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade16.Location = new System.Drawing.Point(688, 288);
             this.Upgrade16.Name = "Upgrade16";
             this.Upgrade16.Size = new System.Drawing.Size(100, 40);
-            this.Upgrade16.TabIndex = 13;
+            this.Upgrade16.TabIndex = 16;
             this.Upgrade16.Text = "Upgrade 16";
             this.Upgrade16.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade16.UseVisualStyleBackColor = true;
+            this.Upgrade16.Click += new System.EventHandler(this.Upgrade16_Click);
             // 
             // Upgrade11
             // 
             this.Upgrade11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade11.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade11.Location = new System.Drawing.Point(688, 58);
             this.Upgrade11.Name = "Upgrade11";
             this.Upgrade11.Size = new System.Drawing.Size(100, 40);
-            this.Upgrade11.TabIndex = 12;
+            this.Upgrade11.TabIndex = 11;
             this.Upgrade11.Text = "Upgrade 11";
             this.Upgrade11.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade11.UseVisualStyleBackColor = true;
+            this.Upgrade11.Click += new System.EventHandler(this.Upgrade11_Click);
             // 
             // Upgrade10
             // 
             this.Upgrade10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Upgrade10.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade10.Location = new System.Drawing.Point(688, 12);
             this.Upgrade10.Name = "Upgrade10";
             this.Upgrade10.Size = new System.Drawing.Size(100, 40);
-            this.Upgrade10.TabIndex = 11;
+            this.Upgrade10.TabIndex = 10;
             this.Upgrade10.Text = "Upgrade 10";
             this.Upgrade10.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Upgrade10.UseVisualStyleBackColor = true;
+            this.Upgrade10.Click += new System.EventHandler(this.Upgrade10_Click);
             // 
             // timer1
             // 
--- ClickerGame/ClickerGame/Form1.cs
+++ ClickerGame/ClickerGame/Form1.cs
@@ -17,12 +17,12 @@ namespace ClickerGame
         // The variable that stores the points the user have
         // and how many points per click they get.
         int points = 0;
-        int pointsPerClick = 1;
+        int pointsPerClickBase = 1;
+        int pointsPerClickMultiplicand = 1;
 
         int upgradePrice;
-        int autoPointsPerSecond = 1;
+        int autoPointsPerSecond;
 
-        bool isDone = false;
         bool[] upgradesBought = new bool[18];
 
         public Form1()
@@ -30,15 +30,46 @@ namespace ClickerGame
             InitializeComponent();
         }
 
+        public void SimpleActiveAnimation( Button button)
+        {
+            // Må endra tallet i Upgrade1 til den knappen som du vill skal ha animasjonen
+            button.Font = new Font("Arial", 20, FontStyle.Bold);
+            if (button.Text != "•••••")
+            {
+                button.Text += "•";
+            }
+            else
+            {
+                button.Text = "";
+            }
+        }
+
         // Increases the points by pointsPerClick and displayes
         // the points in ClickerCounter labes text when the button is clicked. 
         private void ClickerButton_Click(object sender, EventArgs e)
         {
-            points += pointsPerClick;
+            points += (pointsPerClickBase * pointsPerClickMultiplicand);
             ClickerCounter.Text = points.ToString();
         }
 
-        // If points is bigge ror equals upgradePrice you start the AutoPoints funksjon.
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (upgradesBought[0])
+            {
+                points += autoPointsPerSecond;
+                ClickerCounter.Text = points.ToString();
+
+                SimpleActiveAnimation(Upgrade1);
+            }
+            if (upgradesBought[3])
+            {
+                SimpleActiveAnimation(Upgrade4);
+            }
+        }
+
+        // If points is bigge or equals upgradePrice you buy the upgrade
+        // and start does what the upgrade is.
+        // -------------------------------------------------------------
         private void Upgrade1_Click(object sender, EventArgs e)
         {
             upgradePrice = 10;
@@ -50,18 +81,20 @@ namespace ClickerGame
                 upgradesBought[0] = true;
                 Upgrade1.Text = "";
                 Upgrade1.Enabled = false;
+
+                autoPointsPerSecond += 1;
             }
         }
 
         private void Upgrade2_Click(object sender, EventArgs e)
         {
-            upgradePrice = 10;
+            upgradePrice = 1;
 
             if (points >= upgradePrice)
             {
                 points -= upgradePrice;
                 ClickerCounter.Text = points.ToString();
-                pointsPerClick *= 2;
+                pointsPerClickMultiplicand = 2;
                 upgradesBought[1] = true;
                 Upgrade2.Enabled = false;
             }
@@ -75,29 +108,197 @@ namespace ClickerGame
             {
                 points -= upgradePrice;
                 ClickerCounter.Text = points.ToString();
-                pointsPerClick *= 3;
+                pointsPerClickBase *= 3;
                 upgradesBought[2] = true;
                 Upgrade3.Enabled = false;
             }
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void Upgrade4_Click(object sender, EventArgs e)
         {
-            if (upgradesBought[0])
+            upgradePrice = 2;
+
+            if (points >= upgradePrice)
             {
-                points += autoPointsPerSecond;
+                points -= upgradePrice;
                 ClickerCounter.Text = points.ToString();
+                upgradesBought[0] = true;
+                Upgrade4.Text = "";
+                Upgrade4.Enabled = false;
 
-
-                if (Upgrade1.Text != ".....")
-                {
-                    Upgrade1.Text += ".";
-                }
-                else
-                {
-                    Upgrade1.Text = "";
-                }
+                autoPointsPerSecond += 3;
             }
         }
+
+        private void Upgrade5_Click(object sender, EventArgs e)
+        {
+            upgradePrice = 10;
+
+            if (points >= upgradePrice)
+            {
+                points -= upgradePrice;
+                ClickerCounter.Text = points.ToString();
+                upgradesBought[0] = true;
+                Upgrade5.Enabled = false;
+            }
+        }
+        private void Upgrade6_Click(object sender, EventArgs e)
+        {
+            upgradePrice = 10;
+
+            if (points >= upgradePrice)
+            {
+                points -= upgradePrice;
+                ClickerCounter.Text = points.ToString();
+                upgradesBought[0] = true;
+                Upgrade6.Enabled = false;
+            }
+        }
+        private void Upgrade7_Click(object sender, EventArgs e)
+        {
+            upgradePrice = 10;
+
+            if (points >= upgradePrice)
+            {
+                points -= upgradePrice;
+                ClickerCounter.Text = points.ToString();
+                upgradesBought[0] = true;
+                Upgrade7.Enabled = false;
+            }
+        }
+        private void Upgrade8_Click(object sender, EventArgs e)
+        {
+            upgradePrice = 10;
+
+            if (points >= upgradePrice)
+            {
+                points -= upgradePrice;
+                ClickerCounter.Text = points.ToString();
+                upgradesBought[0] = true;
+                Upgrade8.Enabled = false;
+            }
+        }
+        private void Upgrade9_Click(object sender, EventArgs e)
+        {
+            upgradePrice = 10;
+
+            if (points >= upgradePrice)
+            {
+                points -= upgradePrice;
+                ClickerCounter.Text = points.ToString();
+                upgradesBought[0] = true;
+                Upgrade9.Enabled = false;
+            }
+        }
+        private void Upgrade10_Click(object sender, EventArgs e)
+        {
+            upgradePrice = 10;
+
+            if (points >= upgradePrice)
+            {
+                points -= upgradePrice;
+                ClickerCounter.Text = points.ToString();
+                upgradesBought[0] = true;
+                Upgrade10.Enabled = false;
+            }
+        }
+        private void Upgrade11_Click(object sender, EventArgs e)
+        {
+            upgradePrice = 10;
+
+            if (points >= upgradePrice)
+            {
+                points -= upgradePrice;
+                ClickerCounter.Text = points.ToString();
+                upgradesBought[0] = true;
+                Upgrade11.Enabled = false;
+            }
+        }
+        private void Upgrade12_Click(object sender, EventArgs e)
+        {
+            upgradePrice = 10;
+
+            if (points >= upgradePrice)
+            {
+                points -= upgradePrice;
+                ClickerCounter.Text = points.ToString();
+                upgradesBought[0] = true;
+                Upgrade12.Enabled = false;
+            }
+        }
+        private void Upgrade13_Click(object sender, EventArgs e)
+        {
+            upgradePrice = 10;
+
+            if (points >= upgradePrice)
+            {
+                points -= upgradePrice;
+                ClickerCounter.Text = points.ToString();
+                upgradesBought[0] = true;
+                Upgrade13.Enabled = false;
+            }
+        }
+        private void Upgrade14_Click(object sender, EventArgs e)
+        {
+            upgradePrice = 10;
+
+            if (points >= upgradePrice)
+            {
+                points -= upgradePrice;
+                ClickerCounter.Text = points.ToString();
+                upgradesBought[0] = true;
+                Upgrade14.Enabled = false;
+            }
+        }
+        private void Upgrade15_Click(object sender, EventArgs e)
+        {
+            upgradePrice = 10;
+
+            if (points >= upgradePrice)
+            {
+                points -= upgradePrice;
+                ClickerCounter.Text = points.ToString();
+                upgradesBought[0] = true;
+                Upgrade15.Enabled = false;
+            }
+        }
+        private void Upgrade16_Click(object sender, EventArgs e)
+        {
+            upgradePrice = 10;
+
+            if (points >= upgradePrice)
+            {
+                points -= upgradePrice;
+                ClickerCounter.Text = points.ToString();
+                upgradesBought[0] = true;
+                Upgrade16.Enabled = false;
+            }
+        }
+        private void Upgrade17_Click(object sender, EventArgs e)
+        {
+            upgradePrice = 10;
+
+            if (points >= upgradePrice)
+            {
+                points -= upgradePrice;
+                ClickerCounter.Text = points.ToString();
+                upgradesBought[0] = true;
+                Upgrade17.Enabled = false;
+            }
+        }
+        private void Upgrade18_Click(object sender, EventArgs e)
+        {
+            upgradePrice = 10;
+
+            if (points >= upgradePrice)
+            {
+                points -= upgradePrice;
+                ClickerCounter.Text = points.ToString();
+                upgradesBought[0] = true;
+                Upgrade18.Enabled = false;
+            }
+        }
+        // -------------------------------------------------------------
+
     }
 }
