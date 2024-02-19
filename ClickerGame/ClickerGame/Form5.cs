2f3e9d76adca867429378dff53f58fd1e797c51c
JohannesAldal-Jobloop <johannes@jobloop.no>
2024-02-19 15:48:47 +0100

    Started on a function to make the Tetrominos

--- ClickerGame/ClickerGame/Form5.cs
+++ ClickerGame/ClickerGame/Form5.cs
@@ -13,17 +13,27 @@ namespace ClickerGame
     public partial class Form5 : Form
     {
         // Arrays with all the start positions of the squares of the Tetrominos.
-        private int[] PiceI = {0,0, 0,1, 0,2, 0,3};
-        private int[] PiceO = {0,0, 0,1, 1,0, 1,1};
-        private int[] PiceT = {0,1, 1,0, 1,1, 1,2};
-        private int[] PiceS = {0,1, 0,2, 1,0, 1,1};
-        private int[] PiceZ = {0,0, 0,1, 1,1, 1,2};
-        private int[] PiceJ = {0,0, 1,0, 1,1, 1,2};
-        private int[] PiceL = {0,2, 1,0, 1,1, 1,2};
+        private int[] piceI = {0,0, 0,1, 0,2, 0,3};
+        private int[] piceO = {0,0, 0,1, 1,0, 1,1};
+        private int[] piceT = {0,1, 1,0, 1,1, 1,2};
+        private int[] piceS = {0,1, 0,2, 1,0, 1,1};
+        private int[] piceZ = {0,0, 0,1, 1,1, 1,2};
+        private int[] piceJ = {0,0, 1,0, 1,1, 1,2};
+        private int[] piceL = {0,2, 1,0, 1,1, 1,2};
 
         public Form5()
         {
             InitializeComponent();
+        }
+
+        private void MakeTetrominoInGridd(int[] piceArray)
+        {
+            for (int i = 0; i < piceArray.Length; i++)
+            {
+                FlowLayoutPanel newBlock = new FlowLayoutPanel();
+
+                // Legg inn ein ny FlowLayoutPanel på neste kordinat i piceArray
+            }
         }
     }
 }
